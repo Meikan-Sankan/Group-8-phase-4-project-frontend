d915b1a2e3f8b27fe9fb2b059bec0b45689c065d
Meikan-Sankan <meikanseikan@gmail.com>
2024-07-16 21:37:07 +0300

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import './login.css'; // Import the CSS file
 
 const Login = ({ onLogin, error }) => {
   const [email, setEmail] = useState('');
@@ -32,39 +33,37 @@ const Login = ({ onLogin, error }) => {
   };
 
   return (
-    <>
-      <section className="login">
-        <h1 className="heading">Login</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="inputBox">
-            <input 
-              type="text" 
-              placeholder="Email" 
-              required 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-            />
-          </div>
-          <div className="inputBox">
-            <input 
-              type="password" 
-              placeholder="Password" 
-              required 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-            />
-          </div>
-          {error && <p className="error">{error}</p>}
-          {success && <p className="success">{success}</p>}
-          <div className="inputBox">
-            <button type="submit" className="btn">Login</button>
-          </div>
-          <div className="inputBox">
-            <p>Don't have an account? <Link to="/register">Register</Link></p>
-          </div>
-        </form>
-      </section>
-    </>
+    <section className="login">
+      <h1 className="heading">Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="inputBox">
+          <input 
+            type="text" 
+            placeholder="Email" 
+            required 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </div>
+        <div className="inputBox">
+          <input 
+            type="password" 
+            placeholder="Password" 
+            required 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </div>
+        {error && <p className="error">{error}</p>}
+        {success && <p className="success">{success}</p>}
+        <div className="inputBox">
+          <button type="submit" className="btn">Login</button>
+        </div>
+        <div className="inputBox">
+          <p>Don't have an account? <Link to="/register">Register</Link></p>
+        </div>
+      </form>
+    </section>
   );
 };
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './register.css'; // Import the CSS file
 
 const Register = ({ onRegister }) => {
   const [username, setUsername] = useState('');
